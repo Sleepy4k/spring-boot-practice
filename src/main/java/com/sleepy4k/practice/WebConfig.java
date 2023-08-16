d61f16136f737c798fa324e5663cee33cc0c8ecc
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-08-16 21:31:48 +0700

    feat: web config

---
+++ src/main/java/com/sleepy4k/practice/WebConfig.java
@@ -0,0 +1,22 @@
+package com.sleepy4k.practice;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+
+import com.sleepy4k.practice.resolver.UserArgumentResolver;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+  @Autowired
+  private UserArgumentResolver userArgumentResolver;
+
+  @Override
+  public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+    WebMvcConfigurer.super.addArgumentResolvers(resolvers);
+    resolvers.add(userArgumentResolver);
+  }
+}
